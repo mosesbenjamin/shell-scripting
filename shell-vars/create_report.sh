9567cdf996e120f3b5e7daeaf82f051ab09c02d8
mosesbenjamin <mavewreek@gmail.com>
2022-05-15 14:48:59 -0600

    End of options- to prevent against unsafe args passed in

--- shell-vars/create_report.sh
+++ shell-vars/create_report.sh
@@ -5,7 +5,7 @@
 container="$1"
 directory="$2"
 
-mkdir -p "$directory"
-grep "$container" shipments.csv > "$directory/$container.csv"
+mkdir -p -- "$directory"
+grep -- "$container" shipments.csv > "$directory/$container.csv"
 
 echo Wrote report "$directory/$container.csv"
