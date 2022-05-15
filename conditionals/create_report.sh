2d096b8315c7f19e4a8f3466e2ad248977a9e23f
mosesbenjamin <mavewreek@gmail.com>
2022-05-15 15:37:42 -0600

    Conditional expressions

--- conditionals/create_report.sh
+++ conditionals/create_report.sh
@@ -2,15 +2,30 @@
 
 # Create a report file for a single shipping container
 
+if [[ ! $input_file ]]; then
+    echo "Error: Input file not configured"
+    exit 1
+fi
+
+if [[ -e $input_file ]]; then
+    echo "Error: Input file $input_file does not exist"
+    exit 1
+fi
+
 if [[ ! $1 ]]; then
     echo "missing parameter: container name"
     exit 1
 fi
-
 container="$1"
-directory="$2"
 
-mkdir -p -- "$directory"
+if [[ $2 ]]; then
+    directory="$2"
+else
+    directory="$HOME/reports"
+fi
+
+mkdir -p -- "$directory"Creating and Running a Shell Script
+
 if grep -- "$container" "$input_file" > "$directory/{$container}_report.csv"
 then
     echo Wrote report "$directory/container.csv"
